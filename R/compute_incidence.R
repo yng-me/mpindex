9346b6a3644678a6fe7cd40c08264f9d3a79106f
Bhas Abdulsamad <aeabdulsamad@gmail.com>
2023-09-05 21:09:42 +0800

    wip

---
+++ R/compute_incidence.R
@@ -0,0 +1,15 @@
+compute_incidence <- function(.data, ...) {
+
+  n <- NULL
+  .pattern_str <- '^d\\d{2}_i\\d{2}.*'
+
+  df <- .data |>
+    dplyr::group_by(...) |>
+    dplyr::add_count() |>
+    dplyr::ungroup() |>
+    dplyr::group_by(..., n) |>
+    dplyr::summarise_at(dplyr::vars(dplyr::matches(.pattern_str)), mean, na.rm = T) |>
+    dplyr::select(..., n, dplyr::matches(.pattern_str))
+
+  return(df)
+}
