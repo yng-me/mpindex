0b8b2b0b8ca8778296b0b96a84f89a3a507fcc26
Bhas Abdulsamad <aeabdulsamad@gmail.com>
2023-09-06 01:33:02 +0800

    wip

--- tests/testthat/test-compute_mpi.R
+++
@@ -1,7 +0,0 @@
-# df <- openxlsx::read.xlsx('./tests/data/mpi-define-sample.xlsx')
-# jsonlite::write_json(df, './tests/data/mpi-define-sample.json', pretty = T)
-
-
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
-})
