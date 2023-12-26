01b143a37d0129bb32a36ea6d32f3f07e714cde5
Bhas Abdulsamad <bhasabdulsamad@Bhass-Laptop.local>
2023-12-26 19:36:43 +0800

    fixed failed test

--- tests/testthat/test-compute-contribution.R
+++ tests/testthat/test-compute-contribution.R
@@ -21,21 +21,23 @@ test_that("contribution by dimension works correctly", {
 })
 
 
-# test_that("contribution by dimension works correctly with aggregation", {
-#   mpi_specs <- use_global_mpi_specs(.uid = 'uuid', .aggregation = 'class')
-#   dm <- df_household |>
-#     create_deprivation_matrix(deprivation_profile, .mpi_specs = mpi_specs)
-#
-#   hr <- dm$censored |>
-#     compute_headcount_ratio(.aggregation = attr_spec$aggregation)
-#
-#   m_0 <- dm$censored |>
-#     compute_headcount_ratio_adjusted(.aggregation = attr_spec$aggregation) |>
-#     dplyr::select(MPI) |>
-#     dplyr::bind_cols(hr) |>
-#     compute_contribution(.mpi_specs = mpi_specs)
-#
-#   expect_equal(nrow(m_0), 2)
-#   expect_equal(ncol(m_0), 12)
-#
-# })
+test_that("contribution by dimension works correctly with aggregation", {
+  mpi_specs <- use_global_mpi_specs(.uid = "uuid", .aggregation = "class")
+  attr_spec <- attributes(mpi_specs)
+
+  dm <- df_household |>
+    create_deprivation_matrix(deprivation_profile, .mpi_specs = mpi_specs)
+
+  hr <- dm$censored |>
+    compute_headcount_ratio(.aggregation = attr_spec$aggregation)
+
+  m_0 <- dm$censored |>
+    compute_headcount_ratio_adjusted(.aggregation = attr_spec$aggregation) |>
+    dplyr::select(mpi) |>
+    dplyr::bind_cols(hr) |>
+    compute_contribution(.mpi_specs = mpi_specs)
+
+  expect_equal(nrow(m_0), 2)
+  expect_equal(ncol(m_0), 12)
+
+})
